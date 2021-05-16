c773ca091a7c2cf556e059b4c2da7ede9874ac32
Peyman Majidi Moein <peyman.majidi@gmail.com>
2021-05-16 15:41:47 +0430

    first commit

---
+++ moon.bat
@@ -0,0 +1,6 @@
+@echo off
+c:
+cd C:\Users\Peyman\source\repos\code_lovers
+python moon.py
+
+pause>nul
---
+++ moon.py
@@ -0,0 +1,29 @@
+import pylunar, datetime
+
+# City lati/long in DMS format
+TEHRAN_LAT = (35, 41, 21.308)
+TEHRAN_LONG = (51,23,22.561)
+
+def get_shape_moon(phase):
+    """
+    get shape of moon by passing phase
+
+    :phase: current moon phase
+    :return: string contain shape of moon
+    """ 
+    with open('moon.txt', 'r') as file:
+        lines = file.readlines()        
+        index = [lines.index(line) for line in lines if phase in line][0]
+        return ''.join(lines[index-2:index+4])
+        
+                
+     
+moon = pylunar.MoonInfo(TEHRAN_LAT, TEHRAN_LONG)
+today = datetime.datetime.now()
+
+moon.update((today.year, today.month, today.day+10, today.hour, today.minute, 0))
+
+# normalize the phase name by removing '_' 
+phase = moon.phase_name().replace('_', ' ')
+
+print(get_shape_moon(phase))
